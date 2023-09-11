e6ab4926f114a659c55bc94547b4175cde7376c0
Julian <savageman86@gmail.com>
2023-09-11 21:32:20 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,71 @@
+
+
+document.querySelectorAll('a[href^=\\#]').forEach((el) => {
+  el.addEventListener('click', (e) => {
+    e.stopImmediatePropagation();
+    let hash = e.currentTarget.getAttribute('href');
+    let target = document.querySelector(hash);
+    console.log(e.target.getAttribute('href'), target);
+    if (!target) {
+      return;
+    }
+    e.preventDefault();
+    smoothScroll(target, {behavior: 'smooth', block: 'start'}).then(() => {
+      document.location.hash = hash;
+    })
+  })
+})
+
+/*
+ *
+ * Promised based scrollIntoView( { behavior: 'smooth' } )
+ * @param { Element } elem
+ **  ::An Element on which we'll call scrollIntoView
+ * @param { object } [options]
+ **  ::An optional scrollIntoViewOptions dictionary
+ * @return { Promise } (void)
+ **  ::Resolves when the scrolling ends
+ *
+ */
+function smoothScroll( elem, options ) {
+  return new Promise( (resolve) => {
+    if( !( elem instanceof Element ) ) {
+      throw new TypeError( 'Argument 1 must be an Element' );
+    }
+    let same = 0; // a counter
+    let lastPos = null; // last known Y position
+    // pass the user defined options along with our default
+    const scrollOptions = Object.assign( { behavior: 'smooth' }, options );
+
+    // let's begin
+    elem.scrollIntoView( scrollOptions );
+    requestAnimationFrame( check );
+
+    // this function will be called every painting frame
+    // for the duration of the smooth scroll operation
+    function check() {
+      // check our current position
+      const newPos = elem.getBoundingClientRect().top;
+
+      if( newPos === lastPos ) { // same as previous
+        if(same ++ > 2) { // if it's more than two frames
+          /* @todo: verify it succeeded
+          * if(isAtCorrectPosition(elem, options) {
+          *   resolve();
+          * } else {
+          *   reject();
+          * }
+          * return;
+          */
+          return resolve(); // we've come to an halt
+        }
+      }
+      else {
+        same = 0; // reset our counter
+        lastPos = newPos; // remember our current position
+      }
+      // check again next painting frame
+      requestAnimationFrame(check);
+    }
+  });
+}
